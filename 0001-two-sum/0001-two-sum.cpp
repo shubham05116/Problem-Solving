3eb1fad93688b007ab10683f821845308d250337
Shubham kumar singh <88928071+shubham05116@users.noreply.github.com>
2024-09-14 23:03:17 +0530

    Time: 0 ms (100%), Space: 14.4 MB (22.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,18 +2,33 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int k) {
 
-        //Brute force Approach:
+        // Brute force Approach:
 
-       int n = arr.size();
-       int sum =0;
-       for(int i =0 ; i<n-1; i++){
-        for(int j =i+1; j<n ; j++){
-            sum= arr[i]+arr[j];
-            if(sum==k){
-                return {i ,j};
+        int n = arr.size();
+        //    int sum =0;
+        //    for(int i =0 ; i<n-1; i++){
+        //     for(int j =i+1; j<n ; j++){
+        //         sum= arr[i]+arr[j];
+        //         if(sum==k){
+        //             return {i ,j};
+        //         }
+        //     }
+        //    }
+        //    return {-1 , -1};
+
+        // optimal by using two pointers:
+
+        // first sort the array:
+        unordered_map<int, int> mpp;
+
+        for (int i = 0; i < n; i++) {
+            int num =arr[i];
+            int diff = k-num ;
+            if (mpp.find(diff) != mpp.end()) {
+                return { i, mpp[diff] };
             }
+            mpp[num] = i;
         }
-       }
-       return {-1 , -1};
+        return {-1, -1};
     }
 };
