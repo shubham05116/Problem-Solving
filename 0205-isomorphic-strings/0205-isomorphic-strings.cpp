f4e4f4b67a61fa3892699c2b1ba4eb2eff708694
Shubham kumar singh <88928071+shubham05116@users.noreply.github.com>
2024-09-21 14:35:28 +0530

    Time: 3 ms (90.46%), Space: 8.4 MB (85.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        //  char map_s[256] = { 0 };
-        // char map_t[256] = { 0 };
-        // int len = s.size();
-        // for (int i = 0; i < len; ++i)
-        // {
-        //     map_s[s[i]]++;
-        //     map_t[t[i]]++;
-        //     if (map_s[s[i]]!=map_t[t[i]]) return false;
-        // }
-        // return true;  
+         char map_s[256] = { 0 };
+        char map_t[256] = { 0 };
+        int len = s.size();
+        for (int i = 0; i < len; ++i)
+        {
+            if (map_s[s[i]]!=map_t[t[i]]) return false;
+        
+                        map_t[t[i]] = map_s[s[i]] = i + 1;
+
+        }
+        return true;  
 
 
-        int hash[256]= {0};
-      bool ischarMapped[256] ={0};
-      int n = s.size();
-      for(int i =0 ; i<n  ; i++){
-          if(hash[s[i]]==0 && ischarMapped[t[i]]==0){
-              hash[s[i]]=t[i];
-              ischarMapped[t[i]]=true;
-          }
-      }
-      for(int i =0 ;  i<n ;i++){
-          if(char(hash[s[i]])!=t[i]){
-              return false ;
-          }
-      }
-      return true;
+    //     int hash[256]= {0};
+    //   bool ischarMapped[256] ={0};
+    //   int n = s.size();
+    //   for(int i =0 ; i<n  ; i++){
+    //       if(hash[s[i]]==0 && ischarMapped[t[i]]==0){
+    //           hash[s[i]]=t[i];
+    //           ischarMapped[t[i]]=true;
+    //       }
+    //   }
+    //   for(int i =0 ;  i<n ;i++){
+    //       if(char(hash[s[i]])!=t[i]){
+    //           return false ;
+    //       }
+    //   }
+    //   return true;
 
         
     }
